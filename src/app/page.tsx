dab6b3317f2e43f7b024efd184f0fb75a4c24dc4
lys15 <nguyenc@bgsu.edu>
2025-04-06 01:15:01 -0400

    change home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,22 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.hero}>
-          <h1>Make the Most of your Meal. Fight Waste. Save the Planet.</h1>
-          <p>Track food expiration dates to reduce waste and eat smarter.</p>
+          <blockquote>
+            The U.S. wastes over 120 billion pounds of food every year.
+            </blockquote>;
+          <p>Make the Most of your Meal. Fight Waste. Save the Planet.</p>
         </div>
-
+{/* 
         <section className={styles.mission}>
           <h2>Our mission: Prevent food waste</h2>
           <blockquote>
             “1/3 of all food produced is wasted globally. Most of it could’ve
             been eaten.”
           </blockquote>
-          <div>{/* Chart will be implemented here */}</div>
-        </section>
+          <div>{ "Chart will be implemented here "}</div>
+        </section> */}
 
-        <button className={styles.button}>Start Preserving Food</button>
+        <button className={styles.button}>Start Saving</button>
       </main>
       <button className={styles.addNewButton}>Add New</button>
     </div>
